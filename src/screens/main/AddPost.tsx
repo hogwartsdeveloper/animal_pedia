c8df50ade7b27f3562285d794f5b2446a110d438
Zhannur <jannuraidynuly@gmail.com>
2022-02-25 05:40:32 +0500

    use expo image

--- src/screens/main/AddPost.tsx
+++ src/screens/main/AddPost.tsx
@@ -1,10 +1,37 @@
-import { FC } from "react";
-import { Text, View } from "react-native";
+import { async } from "@firebase/util";
+import { Camera } from "expo-camera";
+import { CameraType } from "expo-camera/build/Camera.types";
+import { FC, useEffect, useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
 
 const AddPost: FC = () => {
+    const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+    const [type, setType] = useState<CameraType>(Camera.Constants.Type.back);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestCameraPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, [])
+
+    if (hasPermission === null) {
+        return <View />;
+    }
+
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
     return (
        <View>
-           <Text>AddPost</Text>
+           <Camera type={type}>
+               <View>
+                   <TouchableOpacity>
+                       <Text> Flip </Text>
+                   </TouchableOpacity>
+               </View>
+           </Camera>
        </View>
     );
 };
