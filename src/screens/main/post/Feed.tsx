1e375d75f792a1887fd25be8427c788551195c2d
Zhannur <jannuraidynuly@gmail.com>
2022-03-13 16:23:58 +0500

    Feed screen

--- src/screens/main/post/Feed.tsx
+++ src/screens/main/post/Feed.tsx
@@ -4,6 +4,8 @@ import { container, utils } from "../../../styles";
 import { IPost } from "../../../type/user";
 import BottomSheet from 'react-native-bottomsheet-reanimated'
 import Loader from "../../../components/Loader";
+import { useActions } from "../../../hooks/useActions";
+import Post from "./Post";
 
 interface IFeedProps {
     feed: IPost;
@@ -12,24 +14,56 @@ interface IFeedProps {
 
 const Feed: FC<IFeedProps> = () => {
     const [posts, setPosts] = useState([]);
-    const [refreshing, setRefreshing] = useState<boolean>(false);
-    const [unmutted, setUnmuted] = useState(null);
-    const [sheetRef, setSheetRef] = useState(useRef(null));
-    const [modalShow, setModalShow] = useState({ visible: false, item: null });
-    const [isValid, setIsValid] = useState(true);
+    const [refreshing, setRefreshing] = useState(false);
+    const [inViewPort, setInViewPort] = useState(0);
 
-    // const onViewableItemsChanged = useRef(({ viewableItems, changed}) => {
-    //     if (changed && changed.length > 0) {
-    //         setViewPort(changed[0].index);
-    //     }
-    // })
+
+    const { clearData, fetchUser, fetchUserPosts, fetchUserFollowing} = useActions();
+
+    const reload = () => {
+        clearData(),
+        fetchUser(),
+        fetchUserPosts(),
+        fetchUserFollowing()
+    };
+
+    const onViewableItemsChanged = useRef(( changed: any) => {
+        if (changed && changed.length > 0) {
+            setInViewPort(changed[0].index);
+        }
+    })
 
     return (
-    
-    <View>
-        <Loader />
-    </View>
-    );
+        <View style={[container.container, utils.backgroundWhite]}>
+            <FlatList 
+                refreshControl={
+                    <RefreshControl 
+                        refreshing={refreshing}
+                        onRefresh={() => {
+                            setRefreshing(true);
+                            reload()
+                        }}
+                    />
+                }
+
+                onViewableItemsChanged={onViewableItemsChanged.current}
+                viewabilityConfig={{
+                    waitForInteraction: false,
+                    viewAreaCoveragePercentThreshold: 70
+                }}
+                numColumns={1}
+                horizontal={false}
+                data={posts}
+                keyExtractor={(item, index) => index.toString()}
+
+                renderItem={({ item, index}) => (
+                    <View key={index}>
+                        <Text>Test</Text>
+                    </View>
+                )}
+            />
+        </View>
+    )
 };
 
 export default Feed;
