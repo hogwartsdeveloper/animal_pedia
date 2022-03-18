f0fb5436199c78d793efb4d24a07e491925c85dc
Zhannur <jannuraidynuly@gmail.com>
2022-03-19 02:11:22 +0500

    Dashboard stil

--- src/screens/main/Dashboard.tsx
+++ src/screens/main/Dashboard.tsx
@@ -1,10 +1,38 @@
-import { FC } from "react";
-import { Text, View } from "react-native";
+import { FC, useEffect } from "react";
+import { Button, FlatList, Text, TouchableOpacity, View } from "react-native";
+import { useTypedSelector } from "../../hooks/useTypedSelector";
+import { container, utils } from "../../styles";
+import { dashboardProps } from "../../type";
+import CachedImage from "./random/CachedImage";
 
-const Dashboard: FC = () => {
+const Dashboard: FC<dashboardProps> = ({ navigation }) => {
+    const { posts } = useTypedSelector(state => state.usersState);
+
+    useEffect(() => {
+        console.log(posts);
+    }, [posts])
     return (
-        <View>
-            <Text>Dashboard</Text>
+        <View style={[container.container, utils.backgroundWhite]}>
+            <FlatList 
+                numColumns={1}
+                horizontal={false}
+                data={posts}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item}) => (
+                    <>
+                        <TouchableOpacity
+                            style={[container.containerImage, utils.borderWhite]}
+                            onPress={() => navigation.navigate('Post', {item})}
+                        >
+                            <CachedImage sourse={item.downloadURL} cacheKey={item.id} styles={container.imagePost} />
+                        </TouchableOpacity>
+                        <View>
+                            <Button title="Утвердить" onPress={() => console.log('Approve')} />
+                            <Button title="Удалить" onPress={() => console.log('Удалить')} />
+                        </View>
+                    </>
+                )}
+            />
         </View>
     )
 }
